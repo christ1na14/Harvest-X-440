40ea860888562a27dd9aa85c57fad7b794fefff3
thejoshbailey <48526845+thejoshbailey@users.noreply.github.com>
2019-05-10 19:29:06 -0400

    final files

--- upload.py
+++ upload.py
@@ -72,12 +72,18 @@ app_worksheet = app_workbook.sheet_by_index(0)
 part_dict = {}
 for i in range(1, part_worksheet.nrows):
     row = part_worksheet.row_values(i)
-    # value_row_int = int(row[1])
     value_row = row[5]
     date_row = row[0]
     part_dict[i] = value_row
-    # part_dict[i] = date_row
 
+month_dict = {}
+for i in range(1, part_worksheet.nrows):
+    row = part_worksheet.row_values(i)
+    # value_row_int = int(row[1])
+    month_value_row = row[5]
+    month_date_row = row[0]
+    month_new_dates = month_date_row[5:7]
+    month_dict[i] = month_value_row, month_new_dates
 
 # for loop to start at the 2nd row of the excel file and add the index (key) and ID (value) and add to dictionary
 app_dict = {}
@@ -85,12 +91,11 @@ for i in range(1, app_worksheet.nrows):
     row = app_worksheet.row_values(i)
     # dictionary index should be your key (whatever row you want, has to be unique), the appending varibale will be
     # your value (whatever row)
-    app_dict[row[7]] = row[8]
+    app_dict[row[6]] = row[7]
 
 date_dict = {}
 for i in range(1, part_worksheet.nrows):
     row = part_worksheet.row_values(i)
-    # value_row_int = int(row[1])
     date_row = row[0]
     new_dates = date_row[5:7]
     date_dict[row[5]] = new_dates
@@ -112,6 +117,11 @@ for key, value in date_dict.items():
     rev_date_workbook.setdefault(value, set()).add(key)
 rev_date_dict = [key for key, values in rev_date_workbook.items() if len(values) > 1]
 
+rev_month_workbook = {}
+for key, value in month_dict.items():
+    rev_month_workbook.setdefault(value, set()).add(key)
+rev_month_dict = [key for key, values in rev_month_workbook.items() if len(values) > 1]
+
 address_dict = {}
 for key in rev_part_workbook:
     if key in app_dict:
@@ -121,6 +131,11 @@ rev_address_workbook = {}
 for key, value in address_dict.items():
     rev_address_workbook.setdefault(value, set()).add(key)
 rev_address_dict = [key for key, values in rev_address_workbook.items() if len(values) > 1]
+
+month_hh = {}
+for key in date_dict:
+    if key in address_dict:
+        month_hh[address_dict[key]] = date_dict[key]
 
 ############ CALCULATIONS ###################
 # calculations to determine duplicates
@@ -133,32 +148,184 @@ ind_hh = len(rev_address_workbook)
 print("<div class='topnav'><a href='/Harvest-X/'>Home</a></div>")
 print('<div align=''center>')
 print('<br>')
-print('<h2>Individual Households:<small class="text-muted">', 90, '</small></h2><br>')
+print('<h2>Individual Households:<small class="text-muted">', ind_hh, '</small></h2><br>')
 print('<h2>Total Participants:<small class="text-muted">', total_part, '</small></h2><br>')
 print('<h2>Unique Individuals:<small class="text-muted">', ind_part, '</small></h2><br>')
-print('<h2>Unique Individuals By Month:</h2><br>')
-print('<h2>February: <small class="text-muted">', 88, '</small></h2><br>')
-print('<h2>March: <small class="text-muted">', 29, '</small></h2><br>')
-print('<h2>April: <small class="text-muted">', 37, '</small></h2><br>')
+print('<h2>Unique Households By Month:</h2><br>')
+jan_hh_count = 0
+for key, value in month_hh.items():
+    if value == '01':
+        jan_hh_count += 1
+if jan_hh_count != 0:
+    print('<h2><small class="text-muted">January:', jan_hh_count, '</small></h2><br>')
+feb_hh_count = 0
+for key, value in month_hh.items():
+    if value == '02':
+        feb_hh_count += 1
+if feb_hh_count != 0:
+    print('<h2><small class="text-muted">February', feb_hh_count, "</small></h2><br>")
+mar_hh_count = 0
+for key, value in month_hh.items():
+    if value == '03':
+        mar_hh_count += 1
+if mar_hh_count != 0:
+    print('<h2><small class="text-muted">March', mar_hh_count, "</small></h2><br>")
+apr_hh_count = 0
+for key, value in month_hh.items():
+    if value == '04':
+        apr_hh_count += 1
+if apr_hh_count != 0:
+    print('<h2><small class="text-muted">April', apr_hh_count, "</small></h2><br>")
+may_hh_count = 0
+for key, value in month_hh.items():
+    if value == '05':
+        may_hh_count += 1
+if may_hh_count != 0:
+    print('<h2><small class="text-muted">May', may_hh_count, "</small></h2><br>")
+jun_hh_count = 0
+for key, value in month_hh.items():
+    if value == '06':
+        jun_hh_count += 1
+if jun_hh_count != 0:
+    print('<h2><small class="text-muted">June', jun_hh_count, "</small></h2><br>")
+jul_hh_count = 0
+for key, value in month_hh.items():
+    if value == '07':
+        jul_hh_count += 1
+if jul_hh_count != 0:
+    print('<h2><small class="text-muted">July', jul_hh_count, "</small></h2><br>")
+aug_hh_count = 0
+for key, value in month_hh.items():
+    if value == '08':
+        aug_hh_count += 1
+if aug_hh_count != 0:
+    print('<h2><small class="text-muted">August', aug_hh_count, "</small></h2><br>")
+sep_hh_count = 0
+for key, value in month_hh.items():
+    if value == '09':
+        sep_hh_count += 1
+if sep_hh_count != 0:
+    print('<h2><small class="text-muted">September', sep_hh_count, "</small></h2><br>")
+oct_hh_count = 0
+for key, value in month_hh.items():
+    if value == '10':
+        oct_hh_count += 1
+if oct_hh_count != 0:
+    print('<h2><small class="text-muted">October', oct_hh_count, "</small></h2><br>")
+nov_hh_count = 0
+for key, value in month_hh.items():
+    if value == '11':
+        nov_hh_count += 1
+if nov_hh_count != 0:
+    print('<h2><small class="text-muted">November', nov_hh_count, "</small></h2><br>")
+dec_hh_count = 0
+for key, value in month_hh.items():
+    if value == '12':
+        dec_hh_count += 1
+if dec_hh_count != 0:
+    print('<h2><small class="text-muted">December', dec_hh_count, "</small></h2><br>")
+
 print('<h2>Total Individuals By Month:</h2><br>')
-print('<h2>February: <small class="text-muted">', 90, '</small></h2><br>')
-print('<h2>March: <small class="text-muted">', 29, '</small></h2><br>')
-print('<h2>April: <small class="text-muted">', 37, '</small></h2><br>')
+jan_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '01':
+        jan_count += len([item for item in value if item])
+if jan_count != 0:
+    print('<h2><small class="text-muted">January:', jan_count, "</small></h2><br>")
+
+feb_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '02':
+        feb_count += len([item for item in value if item])
+if feb_count != 0:
+    print('<h2><small class="text-muted">February:', feb_count, "</small></h2><br>")
+
+mar_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '03':
+        mar_count += len([item for item in value if item])
+if mar_count != 0:
+    print('<h2><small class="text-muted">March:', mar_count, "</small></h2><br>")
+
+apr_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '04':
+        apr_count += len([item for item in value if item])
+if apr_count != 0:
+    print('<h2><small class="text-muted">April:', apr_count, "</small></h2><br>")
+
+may_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '05':
+        may_count += len([item for item in value if item])
+if may_count != 0:
+    print('<h2><small class="text-muted">May:', may_count, "</small></h2><br>")
+
+jun_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '06':
+        jun_count += len([item for item in value if item])
+if jun_count != 0:
+    print('<h2><small class="text-muted">June:', jun_count, "</small></h2><br>")
+
+jul_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '07':
+        jul_count += len([item for item in value if item])
+if jul_count != 0:
+    print('<h2><small class="text-muted">July:', jul_count, "</small></h2><br>")
+
+aug_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '08':
+        aug_count += len([item for item in value if item])
+if aug_count != 0:
+    print('<h2><small class="text-muted">August:', aug_count, "</small></h2><br>")
+
+sep_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '09':
+        sep_count += len([item for item in value if item])
+if sep_count != 0:
+    print('<h2><small class="text-muted">September:', sep_count, "</small></h2><br>")
+
+oct_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '10':
+        oct_count += len([item for item in value if item])
+if oct_count != 0:
+    print('<h2><small class="text-muted">October:', oct_count, "</small></h2><br>")
+
+nov_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '11':
+        nov_count += len([item for item in value if item])
+if nov_count != 0:
+    print('<h2><small class="text-muted">November:', nov_count, "</small></h2><br>")
+
+dec_count = 0
+for key, value in rev_month_workbook.items():
+    if key[1] == '01':
+        dec_count += len([item for item in value if item])
+if dec_count != 0:
+    print('<h2><small class="text-muted">December:', dec_count, "</small></h2><br>")
+
 print('<h2>Unique House Holds By Month:</h2><br>')
-print('<h2>February: <small class="text-muted">', 27, '</small></h2><br>')
-print('<h2>March: <small class="text-muted">', 34, '</small></h2><br>')
-print('<h2>April: <small class="text-muted">', 29, '</small></h2><br>')
 
-
+month = {'01': 'January:',
+         '02': 'February:',
+         '03': 'March:',
+         '04': 'April:',
+         '05': 'May:',
+         '06': 'June:',
+         '07': 'July:',
+         '08': 'August:',
+         '09': 'September:',
+         '10': 'October:',
+         '11': 'November:',
+         '12': 'December:'}
 for key, value in rev_date_workbook.items():
-    print(key, len([item for item in value if item]))
-print('</div>')
+    if key in month:
+        print('<h2><small class="text-muted">', month[key], len([item for item in value if item]), "</small></h2><br>")
 
-# print("Individual Households:", ind_hh)
-# print("Total Participants:", total_part)
-# print("Unique Individuals:", ind_part)
-# print("###########")
-# print("Unique Individuals By Month:")
-# for key, value in rev_date_workbook.items():
-#     print(key, len([item for item in value if item]))
-# #
+print('</div>')
